84bbf37fe4f27df286ee7e8c7b1cfd58035afa7a
Uriel Lopez <agnes_ury27@hotmail.com>
2018-07-08 11:45:36 -0500

    Agregamos loading

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -18,13 +18,15 @@ export class ProductosService {
 
 
 public cargar_productos() {
-  this.cargando = false;
+  this.cargando = true;
   if ( this.productos.length === 0) {
     this._http.get('https://paginaweb-23757.firebaseio.com/productos_idx.json')
     .subscribe(res => {
       console.log( res.json());
-      this.cargando = false;
-      this.productos = res.json();
+      setTimeout( () => {
+        this.cargando = false;
+        this.productos = res.json();
+      }, 1500);
     });
 }
 
