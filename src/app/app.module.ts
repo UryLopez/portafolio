97a0c70e3ee2fda017a4a824885c070f9ce4ba73
Uriel Lopez <agnes_ury27@hotmail.com>
2018-07-08 01:40:31 -0500

    rutas agregadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// rutas
+import { app_routing } from './app.routes';
+
+
+
+// componentes
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PortafolioComponent } from './components/portafolio/portafolio.component';
 import { AboutComponent } from './components/about/about.component';
 import { ProductoComponent } from './components/producto/producto.component';
+
 
 @NgModule({
   declarations: [
@@ -15,11 +23,11 @@ import { ProductoComponent } from './components/producto/producto.component';
     FooterComponent,
     PortafolioComponent,
     AboutComponent,
-    ProductoComponent,
-    
+    ProductoComponent
   ],
   imports: [
-    BrowserModule
+     BrowserModule,
+     app_routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,30 @@
+import { Routes, RouterModule, provideRoutes, RouteConfigLoadStart, RouteReuseStrategy } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+
+ import { PortafolioComponent } from './components/portafolio/portafolio.component';
+ import {  AboutComponent } from './components/about/about.component';
+ import { ProductoComponent} from './components/producto/producto.component';
+// import { AboutComponent , PortafolioComponent, ProductoComponent  } from './components/index.paginas';
+
+
+const app_routes: Routes = [
+    { path: '', component: PortafolioComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'producto', component: ProductoComponent },
+    // { path: '**', component: 'home' },
+
+    // { path: 'path/:routeParam', component: MyComponent },
+    // { path: 'staticPath', component: ... },
+     { path: '**', component: 'portafolio' },
+    //  { path: 'oldPath', redirectTo: 'portafolio' },
+    // { path: ..., component: ..., data: { message: 'Custom' }
+];
+
+ @NgModule({
+     imports: [RouterModule.forChild(app_routes)],
+     exports: [RouterModule]
+ })
+ export class FeatureRoutingModule {}
+
+export const app_routing = RouterModule.forRoot(app_routes);
