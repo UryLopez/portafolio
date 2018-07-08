411e6883917817f0c3dcb69eb9203402abe03324
Uriel Lopez <agnes_ury27@hotmail.com>
2018-07-08 04:48:21 -0500

    Servicio de Firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -8,12 +8,37 @@ export class InformacionService {
   info: any = {};
   // tslint:disable-next-line:no-inferrable-types
   cargada: boolean = false;
+  // tslint:disable-next-line:no-inferrable-types
+  cargada_sobre_nosotros: boolean = false;
+
+
+  equipo: any[] = [];
 
 constructor( public _http: Http) {
-    this._http.get('assets/data/info.pagina.json')
-    .subscribe(data => {
-      console.log(data.json());
-      this.info = data.json();
-    });
+
+  this.carga_info();
+
+  this.carga_sobre_nosotros();
+
   }
+
+
+public carga_info() {
+  this._http.get('assets/data/info.pagina.json')
+  .subscribe(data => {
+    // console.log(data.json());
+    this.cargada = true;
+    this.info = data.json();
+  });
+}
+
+public carga_sobre_nosotros() {
+  this._http.get('https://paginaweb-23757.firebaseio.com/equipo.json')
+  .subscribe(data => {
+    console.log(data.json());
+    this.cargada_sobre_nosotros = true;
+    this.equipo = data.json();
+  });
+}
+
 }
