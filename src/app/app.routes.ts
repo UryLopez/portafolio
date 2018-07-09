bf4c560461791a92d42f645553536cd73f82a82b
Uriel Lopez <agnes_ury27@hotmail.com>
2018-07-09 00:32:32 -0500

    Terminamos los fixes de la version 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,7 +14,7 @@ const app_routes: Routes = [
     { path: 'home', component: PortafolioComponent },
     { path: 'about', component: AboutComponent },
     { path: 'producto/:id', component: ProductoComponent },
-    {path: 'buscar/:termino', component: SearchComponent}
+    {path: 'buscar/:termino', component: SearchComponent},
     // { path: '**', component: 'home' },
 
     // { path: 'path/:routeParam', component: MyComponent },
