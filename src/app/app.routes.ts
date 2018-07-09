3db9fd0380fb848bd1c38d6998d6f5cd1fa4e138
Uriel Lopez <agnes_ury27@hotmail.com>
2018-07-08 19:08:02 -0500

    Página de producto y ruta lista

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,7 +11,7 @@ import { NgModule } from '@angular/core';
 const app_routes: Routes = [
     { path: 'home', component: PortafolioComponent },
     { path: 'about', component: AboutComponent },
-    { path: 'producto', component: ProductoComponent },
+    { path: 'producto/:id', component: ProductoComponent },
     // { path: '**', component: 'home' },
 
     // { path: 'path/:routeParam', component: MyComponent },
--- src/app/components/producto/producto.component.ts
+++ src/app/components/producto/producto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router/public_api';
 
 @Component({
   selector: 'app-producto',
